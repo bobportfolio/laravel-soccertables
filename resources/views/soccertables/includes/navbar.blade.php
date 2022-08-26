c8f85432cc8034efffac5255a23a0d940ee17eb0
ebob-robert <robert.smith@ebob.uk>
2022-08-26 17:24:38 +0100

    fixed broken link to bobportfolio

--- resources/views/soccertables/includes/navbar.blade.php
+++ resources/views/soccertables/includes/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <a class="navbar-brand" href="http://portfolio.test">bobportfolio.uk</a>
+    <a class="navbar-brand" href="http://bobportfolio.uk">bobportfolio.uk</a>
     <a class="navbar-brand" href="/">soccertables</a>
     <p class="navbar-brand navbar-right">
         <a href="#footer" class="navbar-link">written in laravel</a>
